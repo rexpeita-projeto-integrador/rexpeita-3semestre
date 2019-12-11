bc295ff61b83e176e55beddea016b5e06038a85b
Yuri Portela Teixeira <53837552+YuriPortela@users.noreply.github.com>
2019-12-11 00:52:12 -0300

    cdc01

--- controllers/login_procController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class login_procController extends controller {
-    
-    public function index() {
-        $this->loadTemplate('login_proc');
-    }
-}
--- controllers/myaccountController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class myaccountController extends controller {
-    
-    public function index() {
-        $this->loadTemplate('myaccount');
-    }
-}
